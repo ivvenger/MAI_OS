5c78c863ebff1433500b0de3231343fa450c1eae
ivvenger <vengerir@yandex.ru>
2025-03-05 20:14:49 +0300

    Lab3 creation

---
+++ lab3/src/child.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+
+
+#define MAX_COMMAND_LEN 256
+//область разделяемой памяти
+#define SHM_NAME "/shared_memory"
+//семафоры для синхронизации между процессами
+#define SEM_PARENT "/sem_parent"
+#define SEM_CHILD "/sem_child"
+
+
+typedef struct {
+    char filename[MAX_COMMAND_LEN];
+    char command[MAX_COMMAND_LEN];
+} shared_data_t;
+
+
+int main() {
+    int shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
+    if (shm_fd == -1) {
+        perror("Ошибка shm_open");
+        exit(1);
+    }
+    //устанавливаем размер разделяемой памяти равный размеру созданной структуры
+    ftruncate(shm_fd, sizeof(shared_data_t));
+    //mmap отображает эту область памяти в адресное пространство процесса, shared_mem указывает на область памяти, которую видят и родительский и дочерний процессы
+    shared_data_t *shared_mem = (shared_data_t *)mmap(0, sizeof(shared_data_t), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (shared_mem == MAP_FAILED) {
+        perror("Ошибка mmap");
+        exit(1);
+    }
+    //создаём 2 семафора для уведомления дочернего процесса, что команда введенаб а второй семафор, что результат готов
+    sem_t *sem_parent = sem_open(SEM_PARENT, 0);
+    sem_t *sem_child = sem_open(SEM_CHILD, 0);
+
+
+    while (1) {
+        sem_wait(sem_parent);
+
+
+        if (strcmp(shared_mem->command, "выход") == 0) {
+            printf("Выход из child...\n");
+            break;
+        }
+        //открываем файл, имя которого было передано через shared mem
+        FILE *file = fopen(shared_mem->filename, "a");
+        if (file == NULL) {
+            perror("Ошибка открытия файла");
+            exit(1);
+        }
+
+
+        int num1, num2, result;
+        char *token = strtok(shared_mem->command, " ");
+        num1 = atoi(token);
+        result = num1;
+
+
+        int division_by_zero = 0;
+        while ((token = strtok(NULL, " ")) != NULL) {
+            num2 = atoi(token);
+            if (num2 == 0) {
+                strcpy(shared_mem->command, "Ошибка деления на 0");
+                fprintf(file, "Ошибка деления на 0\n");
+                division_by_zero = 1;
+                break;
+            }
+            result /= num2;
+        }
+
+
+        if (!division_by_zero) {
+            sprintf(shared_mem->command, "%d", result);
+            fprintf(file, "Результат деления: %d\n", result);
+        }
+
+
+        fflush(file);
+        fclose(file);
+        //говорим родительскому процессу, что ребёнок завершил работу
+        sem_post(sem_child);
+    }
+
+
+    munmap(shared_mem, sizeof(shared_data_t));
+    close(shm_fd);
+    sem_close(sem_parent);
+    sem_close(sem_child);
+    return 0;
+}
---
+++ lab3/src/main.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <semaphore.h>
+
+
+#define MAX_COMMAND_LEN 256
+#define SHM_NAME "/shared_memory"
+#define SEM_PARENT "/sem_parent"
+#define SEM_CHILD "/sem_child"
+
+
+typedef struct {
+    char filename[MAX_COMMAND_LEN];
+    char command[MAX_COMMAND_LEN];
+} shared_data_t;
+
+
+int main() {
+    //создаём разделяемую память с правами доступа 0666, где 0 - отсутстиве спец флагов, а 6 = 4 + 2, т.е. чтение плюс запись для владельца, группы и остальных
+    int shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    if (shm_fd == -1) {
+        perror("Ошибка shm_open");
+        exit(1);
+    }
+    //устанавливаем размер разделяемой памяти равный размеру созданной структуры
+    ftruncate(shm_fd, sizeof(shared_data_t));
+    //mmap отображает эту область памяти в адресное пространство процесса, shared_mem указывает на область памяти, которую видят и родительский и дочерний процессы
+    shared_data_t *shared_mem = (shared_data_t *)mmap(0, sizeof(shared_data_t), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    if (shared_mem == MAP_FAILED) {
+        perror("Ошибка mmap");
+        exit(1);
+    }
+    //создаём 2 семафора для уведомления дочернего процесса, что команда введенаб а второй семафор, что результат готов
+    sem_t *sem_parent = sem_open(SEM_PARENT, O_CREAT, 0666, 0);
+    sem_t *sem_child = sem_open(SEM_CHILD, O_CREAT, 0666, 0);
+    //делаем fork и создаём дочерний процесс
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("Ошибка fork");
+        exit(1);
+    }
+    //находимся в дочернем 
+    if (pid == 0) {
+        execl("./child", "child", NULL);
+        perror("Ошибка");
+        exit(1);
+    } else {
+        //внутри родительского
+        printf("Введите имя файла: ");
+        fgets(shared_mem->filename, sizeof(shared_mem->filename), stdin);
+        shared_mem->filename[strcspn(shared_mem->filename, "\n")] = 0;
+
+
+        char command[MAX_COMMAND_LEN];
+        while (1) {
+            printf("Введите числа, разделенные пробелом или 'выход' для завершения: ");
+            //считываем команду из stdin
+            fgets(command, sizeof(command), stdin);
+            command[strcspn(command, "\n")] = 0;
+            //копируем команду в shared memory
+            strcpy(shared_mem->command, command);
+            //увеличиваем счётчик семафора и дочерний процесс может быть выполнен
+            sem_post(sem_parent);
+            //проверка на выход
+            if (strcmp(command, "выход") == 0) {
+                break;
+            }
+            //ждём выполнение дочернего процесса, 
+            sem_wait(sem_child);
+            printf("Результат деления: %s\n", shared_mem->command);
+        }
+
+
+        wait(NULL);
+        //закрываем все семафоры и освобождаем ресурсы
+        sem_close(sem_parent);
+        sem_close(sem_child);
+        sem_unlink(SEM_PARENT);
+        sem_unlink(SEM_CHILD);
+        munmap(shared_mem, sizeof(shared_data_t));
+        shm_unlink(SHM_NAME);
+        close(shm_fd);
+    }
+
+
+    return 0;
+}
